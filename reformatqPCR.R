2cb009442ce7d5400c973d109689773a4405b50e
Melissa Naugle <mnaugle@csumb.edu>
2021-04-26 19:09:25 -0700

    lots of updates

--- reformatqPCR.R
+++ reformatqPCR.R
@@ -3,7 +3,7 @@ rm( list = ls())
 graphics.off()
 library(tidyr)
 
-data <- read.csv("rawdata/08_05_20_ahyac2019_fields1-4_5sites_threshold115.csv", header = F)
+data <- read.csv("rawdata/2014data/2014data_04_26_21_ahyac31-36,38-43_50th.csv", header = F)
 head(data)
 colnames(data) <- as.character(unlist(data[20,]))
 data <- data[-c(1:20),]
@@ -16,17 +16,16 @@ head(data)
 #confirm neg control 
 data %>%
   filter(Content == "Neg Ctrl")
-
 #check that theres no value for these in FAM or VIC columns 
 
 data$Target <- NULL
 data$Content <- NULL
 data$Well <- NULL
+data$`Starting Quantity (SQ)` <- NULL
 
 head(data)
 tail(data)
 
 
 #write CSV
-
-write.csv(data,"cleandata/08_05_20_fields1-4.csv")
+write.csv(data,"cleandata/2014data/2014data_clean_04_26_21_ahyac31-36,38-43_50th.csv", row.names = F)
